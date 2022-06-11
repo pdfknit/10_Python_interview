9c985b702c05a56c4d45a66b0af63e1e96900b11
Sonya Y <yasenkovasofia@gmail.com>
2022-06-11 14:23:22 +0300

    lesson_04

---
+++ review/lessons/urls.py
@@ -0,0 +1,11 @@
+# from django.conf.urls import url
+from lessons.views import ProductListView
+import lessons.views as lessons
+
+from django.urls import path
+
+
+urlpatterns = [
+    path('products/', ProductListView.as_view()),
+    path('products2/', lessons.products, name='product'),
+]
