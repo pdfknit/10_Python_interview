29f5419160234ad9423de8fd74c7e43d59b9df61
Sonya Y <yasenkovasofia@gmail.com>
2022-06-16 14:29:20 +0300

    lesson_05

--- review/lessons/urls.py
+++ review/lessons/urls.py
@@ -8,4 +8,5 @@ from django.urls import path
 urlpatterns = [
     path('products/', ProductListView.as_view()),
     path('products2/', lessons.products, name='product'),
+    path('category/<int:pk>/', lessons.products_in_categories, name='categories'),
 ]
